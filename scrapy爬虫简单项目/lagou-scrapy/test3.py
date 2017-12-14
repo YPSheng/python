44431c7fdb34890fa140a12c1e1863abbb7f29d4
YPS <1093211972@qq.com>
2017-12-14 11:44:15 +0800

    scrapy小项目

--- scrapy爬虫简单项目/lagou-scrapy/test3.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-#
-#
-browser = webdriver.Chrome()
-browser.get('https://www.baidu.com/')
-# import requests
-
-# response = requests.get('http://www.baidu.com')
-# print(response.text)
